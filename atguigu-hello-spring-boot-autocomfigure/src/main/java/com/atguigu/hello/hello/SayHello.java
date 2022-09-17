e7bb67ba57963ee47741ea1f6b861a2182248c8f
wanghong <wh_12151314@163.com>
2022-09-17 15:43:59 +0800

    gitee test

--- atguigu-hello-spring-boot-autocomfigure/src/main/java/com/atguigu/hello/hello/SayHello.java
+++ atguigu-hello-spring-boot-autocomfigure/src/main/java/com/atguigu/hello/hello/SayHello.java
@@ -1,18 +1,7 @@
 package com.atguigu.hello.hello;
 
 public class SayHello {
-    public void test(){
-        System.out.println("hello git");
-    }
-
-    public void test2(){
-        System.out.println("hello world");
-    }
-    public void test45(){
-        System.out.println("hello wanghong");
-    }
-
-    public void test100(){
-        System.out.println("test push");
+    public void  test(){
+        System.out.println("hello everything");
     }
 }
